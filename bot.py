cbe5486d6418717107df0754ea15610e28f8245b
Jonathan Chan <johnkchan252@gmail.com>
2020-08-01 15:17:52 -0400

    Updated Discord.py Version Requirements

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 from discord.ext import commands, tasks
+from dotenv import load_dotenv
 import discord
 import os
 import re
+
+
+load_dotenv()
 
 client = commands.Bot(command_prefix=".")
 client.remove_command('help')
@@ -30,4 +34,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run(os.environ['BOT_TOKEN'])
+client.run(os.getenv('BOT_TOKEN'))
